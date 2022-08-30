4c57fdc2388480e63608e12bfe7622a288622c89
wayne1894 <mose286778@gmail.com>
2022-08-30 12:00:57 +0800

    no message

---
+++ gsap/js/main.js
@@ -0,0 +1,55 @@
+var parallaxInstance //第一頁視差使用
+var scroll_start = false;
+var page_now = 0;//現在的頁次
+var section = document.querySelectorAll("main>section");
+var last_page_index = section.length - 1;//最後一頁
+
+function wheel(e){
+  if(page_now == last_page_index && e.wheelDelta < 0) return
+  if(page_now == last_page_index && section[page_now].scrollTop > 150) return
+  
+  if(scroll_start) return ;
+  scroll_start = true;
+  var page_index = page_now;
+  if (e.wheelDelta > 0) {
+    page_index = page_index - 1;
+  } else {
+    page_index = page_index + 1;
+  }
+  
+  if(page_index < 0) page_index = 0;
+  if(page_index >= last_page_index) page_index = last_page_index;
+
+  move_page(page_index);
+ 
+}
+
+
+function move_page(page_index){
+  gsap.to(window, 0.5, {
+    scrollTo: section[page_index],
+    onStart : function(){
+      
+    },
+    onComplete : function(){
+      page_now = page_index;
+      setTimeout(function(){
+        scroll_start = false;
+      },1000)
+    }
+  });
+}
+
+function set_parallax(){
+  //https://github.com/wagerfield/parallax
+  var section_1 =  document.getElementById("section_1");
+  parallaxInstance =  new Parallax(section_1)
+}
+
+
+window.addEventListener('wheel', wheel);
+document.addEventListener('DOMContentLoaded', (event) => {
+  set_parallax()
+})
+
+
